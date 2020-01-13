fc282658b1c488d703f5670f5ce0892bae6de672
Michael Andreuzza <34077176+michael-andreuzza@users.noreply.github.com>
2020-01-13 11:28:02 +0200

    Add files via upload

---
+++ colors.js/colors.js
@@ -0,0 +1,23 @@
+// Default
+
+export const PrimaryCyanDefault = "hsl(180, 78%, 62%)";
+export const SecondarykGreenDefault = "hsl(69, 68%, 45%)";
+export const OrangeAlertDefault = "hsl(17, 100%, 73%)";
+export const RedErrorDefault = "hsl(344, 96%, 62%)";
+export const PurpleDarkDefault = "hsl(245, 66%, 64%)";
+
+// Default dark
+
+export const PrimaryCyanDarkDefault = "hsl(180, 48%, 47%)";
+export const SecondaryDarkDefault = "rgba(128, 145, 28, 1)";
+export const OrangeDarkAlertDefault = "hsl(17, 46%, 56%)";
+export const RedDarkErrorDefault = "rgba(191, 40, 80, 1)";
+export const PurpleDarkErDefault = "hsl(245, 37%, 48%)";
+
+// Default  hover
+
+export const PrimaryCyanHoverDefault = "hsl(180, 78%, 73%)";
+export const SecondaryHoverDefault = "hsl(68, 55%, 61%)";
+export const OrangeHoverAlertDefault = "hsl(16, 96%, 90%)";
+export const RedHoverErrorDefault = "hsl(344, 96%, 72%)";
+export const PurpleHoverDefault = "hsl(245, 66%, 75%)";
