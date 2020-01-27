bb2318509f934f0c9bb5d1b53f70ff3a50e4ad22
Michael Andreuzza <34077176+michael-andreuzza@users.noreply.github.com>
2020-01-28 01:56:37 +0200

    Add files via upload

---
+++ color_two.js/colors_two.js
@@ -0,0 +1,23 @@
+// Default
+
+export const PrimaryCyanDefault = "hsl(0, 0%, 28%)";
+export const SecondarykGreenDefault = "hsl(129, 41%, 36%)";
+export const OrangeAlertDefault = " hsl(51, 100%, 60%)";
+export const RedErrorDefault = "hsl(0, 47%, 57%)";
+export const PurpleDarkDefault = "hsl(194, 55%, 29%)";
+
+// Default dark
+
+export const PrimaryCyanDarkDefault = "hsl(0, 0%, 40%)";
+export const SecondaryDarkDefault = " hsl(129, 26%, 46%)";
+export const OrangeDarkAlertDefault = "hsl(17, 46%, 56%)";
+export const RedDarkErrorDefault = "hsl(51, 100%, 67%)";
+export const PurpleDarkErDefault = " hsl(194, 32%, 40%)";
+
+// Default  hover
+
+export const PrimaryCyanHoverDefault = "hsl(0, 0%, 20%)";
+export const SecondaryHoverDefault = "hsl(129, 56%, 29%)";
+export const OrangeHoverAlertDefault = "hsl(51, 100%, 55%)";
+export const RedHoverErrorDefault = "hsl(0, 48%, 52%)";
+export const PurpleHoverDefault = "hsl(194, 85%, 21%)";
