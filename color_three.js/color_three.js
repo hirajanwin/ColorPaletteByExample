365942c584eaeef57c06904cfbdc78f37cba5e41
Michael Andreuzza <34077176+michael-andreuzza@users.noreply.github.com>
2020-02-09 19:10:53 +0200

    Add files via upload

---
+++ color_three.js/color_three.js
@@ -0,0 +1,19 @@
+// Default
+
+export const PrimaryDarkDefault = "hsla(240, 100%, 15%, 1)";
+export const SecondarykPinknDefault = "hsla(353, 100%, 74%, 1)";
+export const AccentYellowDefault = "hsla(60, 100%, 75%, 1)";
+
+//Gradient Default
+
+export const BlueGradientkDefault = "180deg, hsla(221, 69%, 59%, 1), hsla(221, 44%, 41%, 1)";
+export const PinkGradientDefault = "180deg, hsla(353, 100%, 74%, 1), hsla(221, 44%, 41%, 1)";
+
+// Default dark
+
+export const PrimaryDarkShade75 = "hsla(240, 100%, 15%, 0.75)";
+export const PrimaryDarkShade50 = "hsla(240, 100%, 15%, 0.5)";
+export const PrimaryDarkShade25 = "hsla(240, 100%, 15%, 0.25)";
+export const SecondaryDarkShade75 = "hsla(353, 100%, 74%, 0.75)";
+
+
