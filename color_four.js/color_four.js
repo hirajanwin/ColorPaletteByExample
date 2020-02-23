960ada9f1627fbabf19abda03416704752e1517a
Michael Andreuzza <34077176+michael-andreuzza@users.noreply.github.com>
2020-02-23 23:28:35 +0200

    Color Palette By Example N4

---
+++ color_four.js/color_four.js
@@ -0,0 +1,44 @@
+// Dark Default & opacity.
+
+export const PrimaryDarkDefault = "hsla(199, 89%, 11%, 1)";
+
+
+// Without  transparency hsla(209, 59%, 17%, 1).
+export const PrimaryDarkDefault = "hsla(199, 89%, 11%, 0,75)";
+
+// Without  transparency hsla(216, 25%, 34%, 1).
+export const PrimaryDarkDefault = "hsla(199, 89%, 11%, 0,50)";
+
+
+
+
+
+// Orange Default & opacity.
+
+export const SecondaryOrangeDefault = "hsla(18, 98%, 43%, 1)";
+
+
+// Without  transparency hsla(14, 77%, 52%, 1).
+export const SecondaryOrangeDefault = "hsla(18, 98%, 43%, 0,75)";
+
+
+
+
+
+// Yellow Default & opacity.
+
+export const AccentYellowDefault = "hsla(36, 100%, 50%, 1)";
+
+
+// Without  transparency hsla(40, 100%, 56%, 1).
+export const AccentYellowDefault = "hsla(36, 100%, 50%, 0,75)";
+
+
+
+//Gradient Default
+
+export const PrimaryGradientkDefault = "(225deg, hsla(223, 100%, 43%, 1) 0.33%, hsla(199, 100%, 51%, 1) 99.56%)";
+
+export const SecondaryGradientDefault = "(88.99deg, hsla(7, 96%, 60%, 1) -2.68%, hsla(35, 88%, 52%, 1) 94.63%)";
+
+
