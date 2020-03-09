0f4b4b05d992131691ece939021e22ed49ffa2e3
Michael Andreuzza <34077176+michael-andreuzza@users.noreply.github.com>
2020-03-10 00:56:05 +0200

    Add files via upload

---
+++ color_five.js/color_five.js
@@ -0,0 +1,47 @@
+// Dark Default & opacity.
+
+
+// Without  transparency hsl(209, 59%, 17%).
+
+$DarkDefault = "hsla(256, 54%, 14%, 1)";
+
+
+export const $DarkDefault =  "hsla(256, 54%, 14%, 75)";
+
+
+export const $DarkDefault =  "hsla(256, 54%, 14%, 50))";
+
+
+export const $DarkDefault =  "hsla(256, 54%, 14%, 25)";
+
+
+
+
+
+// Without  transparency hsl(249, 100%, 71% ).
+
+export const $PrimaryPurpleDefault =  "hsla(249, 100%, 71%, 1)";
+
+
+export const $PrimaryPurpleDefault =  "hsla(249, 100%, 71%, 75)";
+
+
+
+
+
+// Without  transparency hsl191, 100%, 57%).
+
+export const $SecondaryBlueDefault = "hsla(191, 100%, 57%, 1)";
+
+
+export const $SecondaryBlueDefault = "hsla(191, 100%, 57%, 75)";
+
+
+
+
+export const $PrimaryGradientkDefault = "(219.58deg, hsla(353, 100%, 74%, 0.75) 4.23%, hsla(249, 100%, 71%, 1) 99.49%)";
+
+
+export const $SecondaryGradientDefault = "44.22deg, hsla(191, 100%, 57%, 1) -0.35%, hsla(249, 100%, 78%, 1) 100.27%";
+
+
